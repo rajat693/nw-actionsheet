1f4a10c5215edc7b439ac0f2f214532cb5ca3840
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-05-06 17:24:43 +0530

    feat: done wth nativewind setup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import "../global.css";
+import { Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">Open up App.tsx to start working on your app!</Text>
+    </SafeAreaView>
+  );
+}
